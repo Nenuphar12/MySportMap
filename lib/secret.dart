05c681d8f49109e7787a73a3d0be1ecf57c6e80e
Loïc de Porcaro <deporcaro.loic@gmail.com>
2023-03-13 16:25:17 +0100

    chore: moved old stuff here

--- lib/secret.dart
+++ lib/secret.dart
@@ -1,2 +1,2 @@
-const String clientId = '100668';
-const String clientSecret = 'c043f4ba922e8f079b1189f0d6c9f02b221c1e85';
+const String clientId = 'old-clientID';
+const String clientSecret = 'old-clientSecret';
